4dc9d63909a1c9d5d49f9856159a49ddcb517dd3
AristovaAlina <alinaaristova72@gmail.com>
2025-12-11 19:54:21 +0500

    Task4

---
+++ Tyuiu.AristovaAK.Sprint4.Task4.V22.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.AristovaAK.Sprint4.Task4.V22.Lib;
+namespace Tyuiu.AristovaAK.Sprint4.Task4.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] matrix =
+            {
+                { 4, 3, 2, 4, 5 },
+                { 5, 4, 5, 6, 6 },
+                { 6, 2, 3, 6, 5 },
+                { 6, 2, 3, 6, 4 },
+                { 6, 2, 6, 6, 2 },
+            };
+
+            int[,] res =
+            {
+                { 4, 0, 2, 4, 0 },
+                { 0, 4, 0, 6, 6 },
+                { 6, 2, 0, 6, 0 },
+                { 6, 2, 0, 6, 4 },
+                { 6, 2, 6, 6, 2 },
+            };
+
+            CollectionAssert.AreEqual(res, ds.Calculate(matrix));
+
+        }
+    }
+}
--- Tyuiu.AristovaAK.Sprint4.Task4.V22/Program.cs
+++ Tyuiu.AristovaAK.Sprint4.Task4.V22/Program.cs
@@ -1,4 +1,4 @@
-﻿using Tyuiu.AristovaAK.Sprint4.Task3.V30.Lib;
+﻿using Tyuiu.AristovaAK.Sprint4.Task4.V22.Lib;
 internal class Program
 {
     private static void Main(string[] args)
@@ -7,34 +7,37 @@ internal class Program
         Console.Title = "Спринт #4 | Выполнила: Аристова А. К. | СМАРТб-25-1";
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* Спринт #4                                                               *");
-        Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
-        Console.WriteLine("* Задание #3                                                              *");
-        Console.WriteLine("* Вариант #30                                                             *");
+        Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                            *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #22                                                             *");
         Console.WriteLine("* Выполнила: Аристова Алина Константиновна | СМАРТб-25-1                  *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* УСЛОВИЕ:                                                                *");
         Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
-        Console.WriteLine("* статическими значениями в диапазоне от 1 до 6. Найдите максимальный     *");
-        Console.WriteLine("* элемент в третьей строке массива.                                       *");
+        Console.WriteLine("* значениями с клавиатуры в диапазоне от 2 до 6. Заменить нечетные        *");
+        Console.WriteLine("* элементы массива на 0.                                                  *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
 
-        int[,] matrix =
-            {
-                { 2, 4, 3, 5, 1 },
-                { 6, 6, 1, 2, 6 },
-                { 3, 3, 2, 1, 5 },
-                { 6, 4, 1, 3, 3 },
-                { 5, 1, 1, 6, 4 }
-            };
-
-        Console.WriteLine("Исходный массив:");
+        int[,] matrix = new int[5, 5];
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                Console.Write(matrix[i, j] + " ");
+                Console.Write($"Введите {i},{j} элемент массива: ");
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+
+        Console.WriteLine("Исходный массив:");
+        for (int i1 = 0; i1 < matrix.GetLength(0); i1++)
+        {
+            for (int j1 = 0; j1 < matrix.GetLength(1); j1++)
+            {
+                Console.Write($"{matrix[i1, j1],-2}");
+                //Console.Write(matrix[i1, j1] + " ");
             }
             Console.WriteLine();
         }
@@ -43,7 +46,16 @@ internal class Program
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
-        Console.WriteLine("Максимальный элемент в третьей строке массива: " + ds.Calculate(matrix));
+
+        int[,] res = ds.Calculate(matrix);
+        for (int i2 = 0; i2 < res.GetLength(0); i2++)
+        {
+            for (int j2 = 0; j2 < res.GetLength(1); j2++)
+            {
+                Console.Write($"{res[i2, j2],-2}");
+            }
+            Console.WriteLine();
+        }
 
         Console.ReadKey();
     }
